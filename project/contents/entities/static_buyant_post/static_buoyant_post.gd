c78e02d4bfb287615c04ef468df23d3113ee23d9
Severi Vidnäs <severi.vidnas@gmail.com>
2022-01-22 18:10:07 +0200

    Post sinks into ocean once requirements are met

--- project/contents/entities/static_buyant_post/static_buoyant_post.gd
+++ project/contents/entities/static_buyant_post/static_buoyant_post.gd
@@ -7,13 +7,18 @@ onready var _button_prompt := $ButtonPrompt
 var required_items := []
 
 
+onready var _sink_tween := $SinkTween
+
 func _ready() -> void:
 	required_items = _shuffle_items()
+	sink_to_ocean()
 	pass
 
 
 func remove_item_from_required(item) -> void:
 	required_items.erase(item)
+	if required_items.size() <= 0:
+		sink_to_ocean()
 	pass
 
 func toggle_button_prompt(toggle: bool) -> void:
@@ -31,3 +36,15 @@ func _shuffle_items() -> Array:
 			type = ItemData.ITEM_TYPES.values()[randi() % ItemData.ITEM_TYPES.size()]
 		items.append(ItemData.get_item_info_for_type(type))
 	return items
+
+
+func sink_to_ocean() -> void:
+	_sink_tween.interpolate_property(self, "scale:x", 1.0, 0.0, 5.0, Tween.TRANS_CIRC, Tween.EASE_IN)
+	_sink_tween.interpolate_property(self, "scale:y", 1.0, 0.0, 5.0, Tween.TRANS_CIRC, Tween.EASE_IN)
+	_sink_tween.start()
+	pass
+
+
+func _on_SinkTween_tween_all_completed() -> void:
+	queue_free()
+	pass
